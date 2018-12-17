fa21fde47e218cc430330eb6fe3f95e657a32349
gy0p4k <gyorgypakozdi@gmail.com>
2018-12-17 15:30:44 +0100

    Updates

---
+++ precache-manifest.10f570d40f417f9c04b2755f4bd8ea11.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "939001b84cc71e82e856",
+    "url": "/cardgame/static/css/main.96a39466.chunk.css"
+  },
+  {
+    "revision": "939001b84cc71e82e856",
+    "url": "/cardgame/static/js/main.939001b8.chunk.js"
+  },
+  {
+    "revision": "feb4383ae2578fa8eb81",
+    "url": "/cardgame/static/js/1.feb4383a.chunk.js"
+  },
+  {
+    "revision": "4de7129ab9e660fa79bd",
+    "url": "/cardgame/static/js/runtime~main.4de7129a.js"
+  },
+  {
+    "revision": "0a42ff00dc6f5191b741c7e0e3b696f3",
+    "url": "/cardgame/static/media/logo.0a42ff00.svg"
+  },
+  {
+    "revision": "ef1abca23914d7c222032beebdd5a8b7",
+    "url": "/cardgame/static/media/angular.ef1abca2.png"
+  },
+  {
+    "revision": "02fe5fca788ed36be0d50b2f7add91fb",
+    "url": "/cardgame/static/media/d3.02fe5fca.png"
+  },
+  {
+    "revision": "61e03d19cda078e739f58e1a2b6ec2c9",
+    "url": "/cardgame/static/media/jenkins.61e03d19.png"
+  },
+  {
+    "revision": "2348d8db9a617410416b017d622cfa96",
+    "url": "/cardgame/static/media/postcss.2348d8db.png"
+  },
+  {
+    "revision": "10c6499073ca3af85d9f2220cb740e82",
+    "url": "/cardgame/static/media/react.10c64990.png"
+  },
+  {
+    "revision": "a65a699da94ffe2d72a3006093a3db17",
+    "url": "/cardgame/static/media/redux.a65a699d.png"
+  },
+  {
+    "revision": "56cd4b2119c3aa07d3ed88f0a0f3a7a2",
+    "url": "/cardgame/static/media/sass.56cd4b21.png"
+  },
+  {
+    "revision": "3079ecbf79252f900848e50db0d708a4",
+    "url": "/cardgame/static/media/supercharge.3079ecbf.png"
+  },
+  {
+    "revision": "c237b6ce736367cea61d53072938830e",
+    "url": "/cardgame/static/media/ts.c237b6ce.png"
+  },
+  {
+    "revision": "e4c70b27c954179225c4a4baab6bd224",
+    "url": "/cardgame/index.html"
+  }
+];
